bdf73abd11a22b687433f529539498cb77fcd4eb
Manuel Huber <manuel.h87@gmail.com>
2016-05-09 07:24:42 +0200

    Add build.sh command

---
+++ latex/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd "/source"
+find . -mindepth 1 -type d ! -path "./.git/*" -exec mkdir -p "/build/{}" \;
+latexmk --output-directory=/build -f -pdf ${NAME}.tex
+echo "INFO latexmk returned $?"
+
+cd "/"
+find "/build" -type f -name "*.pdf" -exec cp -v "{}" "/result/" \;
