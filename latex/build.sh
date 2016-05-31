157b1812dc88f44d240991f2fdcd1c84536ba7fd
Manuel Huber <manuel.h87@gmail.com>
2016-06-01 01:59:53 +0200

    Integrate Umlet into this image

--- latex/build.sh
+++ latex/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+groupadd -g ${GID} ${USER}
+useradd -g ${GID} -u ${UID} -M -s /bin/bash ${USER} || exit 1
+
 cd "/source"
 find . -mindepth 1 -type d ! -path "./.git/*" -exec mkdir -p "/build/{}" \;
+find . -mindepth 1 -name "*.uxf" -type f -exec /bin/bash -c 'java -jar /usr/bin/umlet -action=convert -format=pdf -filename="/source/${1}" -output="/build${1%.uxf}.pdf"' _ {} \;
 latexmk --output-directory=/build -f -pdf ${NAME}.tex
 echo "INFO latexmk returned $?"
 
