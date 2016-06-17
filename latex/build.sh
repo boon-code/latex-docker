02b734447e31bc617587eb827924504ac3c673b9
Manuel Huber <manuel.h87@gmail.com>
2016-06-17 20:56:59 +0200

    Automatically export Umlet (PDF)
    - Re-factor Dockerfile and main entrypoint

--- latex/build.sh
+++ latex/build.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-groupadd -g ${GID} ${USER}
-useradd -g ${GID} -u ${UID} -M -s /bin/bash ${USER} || exit 1
+_main() {
+	cd "/source"
+	# Create all images
+	find . -mindepth 1 -name "*.uxf" -type f -exec /bin/bash -c 'mkdir -p "$(dirname "/images/$1")"' _ {} \;
+	find . -mindepth 1 -name "*.uxf" -type f -exec /bin/bash -c 'java -jar /usr/bin/umlet -action=convert -format=pdf -filename="/source/${1}" -output="/images/${1%.uxf}.pdf"' _ {} \;
 
-cd "/source"
-find . -mindepth 1 -type d ! -path "./.git/*" -exec mkdir -p "/build/{}" \;
-find . -mindepth 1 -name "*.uxf" -type f -exec /bin/bash -c 'java -jar /usr/bin/umlet -action=convert -format=pdf -filename="/source/${1}" -output="/build${1%.uxf}.pdf"' _ {} \;
-latexmk --output-directory=/build -f -pdf ${NAME}.tex
-echo "INFO latexmk returned $?"
+	# Create empty directories for latexmk
+	find . -mindepth 1 -type d ! -path "./.git/*" -exec mkdir -p "/build/{}" \;
 
-cd "/"
-find "/build" -type f -name "*.pdf" -exec cp -v "{}" "/result/" \;
+	latexmk --output-directory=/build -f -pdf ${NAME}.tex
+	echo "INFO latexmk returned $?"
 
-if [ "$DEBUG" = "1" ]; then
-	cd "/build"
-	exec /bin/bash
+	# Copy resulting PDF
+	cd "/"
+	find "/build" -maxdepth 1 -type f -name "*.pdf" -exec cp -v "{}" "/result/" \;
+
+	if [ "$DEBUG" = "1" ]; then
+		cd "/build"
+		exec /bin/bash
+	fi
+}
+
+if [ "$(id -u)" = "${UID}" ] && [ "$(id -g)" = "${GID}" ]; then
+	_main $@
+else
+	id -g ${USER} >/dev/null 2>/dev/null
+	if [ $? -ne 0 ]; then
+		groupadd -g ${GID} ${USER} || exit 1
+	fi
+	id -u ${USER} >/dev/null 2>/dev/null
+	if [ $? -ne 0 ]; then
+		useradd -g ${GID} -u ${UID} -m -s /bin/bash ${USER} || exit 1
+	fi
+
+	exec su-exec ${USER} /bin/bash /build.sh
 fi
